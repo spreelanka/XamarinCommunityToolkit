df8f44a67e20a4f9db08d5dde24e1c69ab5fe467
spreelanka <jacob@portablefoundations.com>
2022-11-19 18:25:38 -0500

    mauicompat generate+fix

---
+++ src/CommunityToolkit/Xamarin.CommunityToolkit.MauiCompat/Views/Snackbar/Helpers/iOS/SnackbarViews/ActionMessageSnackBarView.ios.cs
@@ -0,0 +1,29 @@
+﻿using System;using Microsoft.Extensions.Logging;
+using CoreGraphics;
+using UIKit;
+using Xamarin.CommunityToolkit.UI.Views.Helpers.iOS.SnackBarViews;
+
+namespace Xamarin.CommunityToolkit.UI.Views.Helpers.iOS
+{
+	class ActionMessageSnackBarView : MessageSnackBarView
+	{
+		public ActionMessageSnackBarView(NativeSnackBar snackBar)
+			: base(snackBar)
+		{
+		}
+
+		protected override void Initialize(CGRect cornerRadius)
+		{
+			base.Initialize(cornerRadius);
+
+			_ = StackView ?? throw new NullReferenceException();
+			const float actionButtonPriority = 1000;
+			foreach (var actionButton in SnackBar.Actions)
+			{
+				actionButton.SetContentHuggingPriority(actionButtonPriority, UILayoutConstraintAxis.Horizontal);
+				actionButton.SetContentCompressionResistancePriority(actionButtonPriority, UILayoutConstraintAxis.Horizontal);
+				StackView.AddArrangedSubview(actionButton);
+			}
+		}
+	}
+}
